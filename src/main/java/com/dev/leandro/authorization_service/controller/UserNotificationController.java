c730f76b3564f0ac304b14e955a16e8440481928
leandro <android.leandro.siqueira@gmail.com>
2024-10-12 10:21:20 -0300

    Fixing to UserNotificationController returns DTO instead of entity with sensitive fields

--- src/main/java/com/dev/leandro/authorization_service/controller/UserNotificationController.java
+++ src/main/java/com/dev/leandro/authorization_service/controller/UserNotificationController.java
@@ -3,12 +3,14 @@ package com.dev.leandro.authorization_service.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev.leandro.authorization_service.dto.UserNotificationDTO;
 import com.dev.leandro.authorization_service.entity.UserNotification;
 import com.dev.leandro.authorization_service.repository.UserNotificationRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/notification")
@@ -20,7 +22,15 @@ public class UserNotificationController {
     }
 
     @GetMapping
-    public List<UserNotification>  getNotification() {
-        return this.repository.findAll();
+    public List<UserNotificationDTO>  getNotification() {
+        List<UserNotification> notifications = this.repository.findAll();
+
+        return notifications.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
     }   
+
+    private UserNotificationDTO convertToDto(UserNotification userNotification){
+        return new UserNotificationDTO(userNotification.getId(), userNotification.getUser().getName(), userNotification.getUser().getEmail(), userNotification.getNotification());
+    }
 }
